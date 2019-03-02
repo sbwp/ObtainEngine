8083e90e5a349338ea40a4f3339aa623aaa5dda4
Austin Barrett <austinbrrtt@gmail.com>
2019-03-02 11:05:19 -0500

    Started over with cleaner version, window opens

--- src/renderer/vulkan/vulkanrenderer.cpp
+++ src/renderer/vulkan/vulkanrenderer.cpp
@@ -1,24 +1,37 @@
 #include "vulkanrenderer.hpp"
 
 namespace Obtain::Graphics::Vulkan {
-	/****** public ******/
-	
+	/******************************************
+	 ***************** public *****************
+	 ******************************************/
 	VulkanRenderer::VulkanRenderer(std::string title,std::array<uint32_t, 3> version)
 		: title(title), version(version)
 	{
-		instance = new Instance(title, version);
+		windowSize = {1600, 900};
+		initWindow();
 	}
 	
 	VulkanRenderer::~VulkanRenderer()
 	{
-		delete(instance);
+		glfwDestroyWindow(window);
+		glfwTerminate();
 	}
 
 	void VulkanRenderer::run()
 	{
-		instance->run();
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
 	}
 
-	/****** private ******/
+	/******************************************
+	 ***************** private *****************
+	 ******************************************/
 	
+	void VulkanRenderer::initWindow() {
+		glfwInit();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // Don't create OpenGL context
+		glfwWindowHint(GLFW_RESIZABLE, false); // Temp: make window not resizable
+		window = glfwCreateWindow(windowSize[0], windowSize[1], title.c_str(), nullptr, nullptr);
+	}
 }
--- src/renderer/vulkan/vulkanrenderer.hpp
+++ src/renderer/vulkan/vulkanrenderer.hpp
@@ -1,8 +1,11 @@
 #ifndef VULKAN_RENDERER_HPP
 #define VULKAN_RENDERER_HPP
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.hpp>
+
 #include "../renderer.hpp"
-#include "instance.hpp"
 
 namespace Obtain::Graphics::Vulkan {
 	class VulkanRenderer : public Renderer {
@@ -12,10 +15,13 @@ namespace Obtain::Graphics::Vulkan {
 		void run();
 
 	private:
-		Instance *instance;
+		std::array<u_int32_t, 2> windowSize;
+		GLFWwindow* window;
+		vk::UniqueInstance instance;
 		std::string title;
 		std::array<uint32_t, 3> version;
-
+		
+		void initWindow();
 	};
 }
 
