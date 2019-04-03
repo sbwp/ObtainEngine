ea380d3700149ad3cae958ddfdf9539e0cfd1ffe
Austin Barrett <austinbrrtt@gmail.com>
2019-04-02 21:58:28 -0400

    Added vertex input state create info

--- src/renderer/vulkan/pipeline.cpp
+++ src/renderer/vulkan/pipeline.cpp
@@ -7,7 +7,13 @@ namespace Obtain::Graphics::Vulkan {
 		Shader *vertShader = new Shader(device, "basic.vert", vk::ShaderStageFlagBits::eVertex);
 		Shader *fragShader = new Shader(device, "basic.frag", vk::ShaderStageFlagBits::eFragment);
 		
-		// TODO: Create Pipeline
+		auto vertexInputStateCreateInfo = vk::PipelineVertexInputStateCreateInfo(
+			vk::PipelineVertexInputStateCreateFlags(),
+			0,
+			nullptr,
+			0,
+			nullptr
+		);
 		
 		delete(vertShader);
 		delete(fragShader);
